89c819e9d90b95e0fd601f93c8529f852d360441
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-21 16:18:39 +0100

    Use janitor to clean names
    Change the names of the variables to follow our naming standards.

--- meteorites/cleaning.R
+++ meteorites/cleaning.R
@@ -15,11 +15,28 @@
 # 6. Order the data by the year of discovery.
 
 # We would also like you to include assertive programming to make sure that:
-# 1. The data has the variable names we expect (“id”, “name”, “mass (g)”, “fall”
-# , “year”, “GeoLocation”).
+# 1. The data has the variable names we expect ("id", "name", "mass (g)", "fall2
+# , "year", "GeoLocation").
 # 2. Latitude and longitude are valid values. (Latitude between -90 and 90, 
 # longitude between -180 and 180).
 
+library(janitor)
 library(tidyverse)
 
+# Read the data into R
 meteorite_landings <- read_csv("data/meteorite_landings.csv")
+
+# check the data
+# dim(meteorite_landings)
+# Rows 45716, Columns 6 
+# head(meteorite_landings)
+names(meteorite_landings)
+
+# Change the names of the variables to follow our naming standards.
+# mass (g) has spaces, GeoLocation is mixed case.
+# Use janitor to clean the names
+meteorite_landings_clean <- meteorite_landings %>% 
+  clean_names() %>% 
+  #names()
+
+meteorite_landings_clean
