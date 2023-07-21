668275245b0b0a2caf5c7425440341be8e41eb30
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-21 16:02:36 +0100

    Initial reading in of data
    1. Read the data into R

--- meteorites/cleaning.R
+++ meteorites/cleaning.R
@@ -1,4 +1,25 @@
 # Author: Lesley Duff
 # Title: Cleans some data on meteorites
 # Date: 2023-07-21
+# Description:
+#   Data comes from NASA and has information on meteorites that have been found 
+# up to the year 2013.
+#
+# Tasks to be completed
+# 1. Read the data into R
+# 2. Change the names of the variables to follow our naming standards.
+# 3. Split in column GeoLocation into latitude and longitude, the new latitude 
+# and longitude columns should be numeric.
+# 4. Replace any missing values in latitude and longitude with zeros.
+# 5. Remove meteorites less than 1000g in weight from the data.
+# 6. Order the data by the year of discovery.
 
+# We would also like you to include assertive programming to make sure that:
+# 1. The data has the variable names we expect (“id”, “name”, “mass (g)”, “fall”
+# , “year”, “GeoLocation”).
+# 2. Latitude and longitudezxc are valid values. (Latitude between -90 and 90, 
+# longitude between -180 and 180).
+
+library(tidyverse)
+
+meteorite_landings <- read_csv("data/meteorite_landings.csv")
