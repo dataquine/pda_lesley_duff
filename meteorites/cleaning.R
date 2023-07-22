d9b31872adb87d7f35aa0b08954d6d60ad6ddedb
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-22 12:29:54 +0100

    Reorgnise cleaning code into reusable function
    Move code into a reusable function so different file paths can be passed in.
    
    Clean up comments and remove task list from top of file

--- meteorites/cleaning.R
+++ meteorites/cleaning.R
@@ -1,92 +1,84 @@
 # Author: Lesley Duff
-# Title: Cleans some data on meteorites
+# Title: Cleans some meteorite landing data
 # Date: 2023-07-21
 # Description:
 #   Data comes from NASA and has information on meteorites that have been found
 # up to the year 2013.
-# Creates cleaned data frame meteorite_landings_clean
-#
-# Tasks to be completed
-# 1. Read the data into R
-# 2. Change the names of the variables to follow our naming standards.
-# 3. Split in column GeoLocation into latitude and longitude, the new latitude
-# and longitude columns should be numeric.
-# 4. Replace any missing values in latitude and longitude with zeros.
-# 5. Remove meteorites less than 1000g in weight from the data.
-# 6. Order the data by the year of discovery.
-
-# We would also like you to include assertive programming to make sure that:
-# 1. The data has the variable names we expect ("id", "name", "mass (g)", "fall2
-# , "year", "GeoLocation").
-# 2. Latitude and longitude are valid values. (Latitude between -90 and 90,
-# longitude between -180 and 180).
+# Trivia: I once held a piece of meteorite in Geology class.
+#     Surprisingly heavy - it was small and dense from all the melted metal.
 
 library(assertr)
 library(janitor)
 library(tidyverse)
 
-# Read the data into R
-meteorite_landings <- read_csv("data/meteorite_landings.csv")
+# Creates cleaned meteorite landings data frame
+process_meteorite_landings_file <- function(path) {
+  # Read the data into R
+  meteorite_landings <- read_csv(path)
 
-# check the data
-# dim(meteorite_landings)
-# Rows 45716, Columns 6
-# head(meteorite_landings)
-# names(meteorite_landings)
+  # check the data
+  # dim(meteorite_landings)
+  # Rows 45716, Columns 6
+  # head(meteorite_landings)
+  # names(meteorite_landings)
 
-# make sure that the data has the variable names we expect
-expected_variable_names <- c(
-  "id", "name", "mass (g)", "fall", "year",
-  "GeoLocation"
-)
+  # make sure that the data has the variable names we expect
+  expected_variable_names <- c(
+    "id", "name", "mass (g)", "fall", "year",
+    "GeoLocation"
+  )
 
-stopifnot(
-  names(meteorite_landings) == expected_variable_names
-)
+  stopifnot(
+    names(meteorite_landings) == expected_variable_names
+  )
 
-# Change the names of the variables to follow our naming standards.
-# mass (g) has spaces, GeoLocation is mixed case.
-# Use janitor to clean the names
-meteorite_landings <- meteorite_landings %>%
-  janitor::clean_names()
+  # names(meteorite_landings)
 
-# names(meteorite_landings)
+  # Split in column GeoLocation into latitude and longitude, the new latitude
+  # and longitude columns should be numeric.
+  # head(meteorite_landings)
+  # geo_location values e.g (50.775, -6.08333)
+  # Need to get rid of brackets and comma
+  # Looks like a pattern for numbers:
+  # optional minus,
+  # 1 or more digits
+  # fullstop
+  # one or more digits
+  meteorite_landings <- meteorite_landings %>%
+    # Change the names of the variables to follow our naming standards.
+    # mass (g) has spaces, GeoLocation is mixed case.
+    # Use janitor to clean the names
+    janitor::clean_names() %>%
+    # Get rid of bracket at start and end
+    mutate(geo_location_new = str_sub(geo_location, start = 2, end = -2)) %>%
+    # e.g. now looks like  "54.21667, -113.02"
 
-# Split in column GeoLocation into latitude and longitude, the new latitude
-# and longitude columns should be numeric.
-# head(meteorite_landings)
-# geo_location values e.g (50.775, -6.08333)
-# Need to get rid of brackets and comma
-# Looks like a pattern for numbers:
-# optional minus,
-# 1 or more digits
-# fullstop
-# one or more digits
-meteorite_landings_clean <- meteorite_landings %>%
-  # Get rid of bracket at start and end
-  mutate(geo_location_new = str_sub(geo_location, start = 2, end = -2)) %>%
-  # e.g. now looks like  "54.21667, -113.02"
+    # ?separate
+    separate(
+      col = "geo_location_new",
+      into = c("latitude", "longitude"),
+      sep = ", "
+    ) %>%
+    # Convert type of columns into numeric
+    # Replace any missing values in latitude and longitude with zeros.
+    mutate(
+      latitude = coalesce(as.numeric(latitude), 0),
+      longitude = coalesce(as.numeric(longitude), 0)
+    ) %>%
+    # Remove meteorites less than 1000g in weight from the data.
+    filter(mass_g >= 1000) %>%
+    # Check that latitude and longitude are valid values.
+    # (latitude between -90 and 90, longitude between -180 and 180).
+    verify(latitude >= -90 & latitude <= 90) %>%
+    verify(longitude >= -180 & longitude <= 180) %>%
+    # N.B. Earlier in testing longitude failed on one row,
+    # index 29436 32789 Meridiani Planum - These coordinates would appear to
+    # be on Mars!
+    # However this row was removed from the data when we added the weight filter
 
-  # ?separate
-  separate(
-    col = "geo_location_new",
-    into = c("latitude", "longitude"),
-    sep = ", "
-  ) %>%
-  # Convert type of columns into numeric
-  # Replace any missing values in latitude and longitude with zeros.
-  mutate(latitude = coalesce(as.numeric(latitude), 0),
-    longitude = coalesce(as.numeric(longitude), 0)) %>%
-  # Remove meteorites less than 1000g in weight from the data.
-  filter(mass_g >= 1000) %>%
-  # Latitude and longitude are valid values.
-  # (Latitude between -90 and 90, longitude between -180 and 180).
-  verify(latitude >= -90 & latitude <= 90) %>%
-  verify(longitude >= -180 & longitude <= 180) %>%
-  #  N.B. Earlier in testing longitude failed on one row,
-  # index 29436 32789 Meridiani Planum - These coordinates would appear to
-  # be on Mars!
-  # However this row was removed from the data when we added the weight filter
+    # Order the data by the year of discovery
+    arrange(year)
+}
 
-  # Order the data by the year of discovery
-  arrange(year)
+# Test call of cleaning function
+#meteorite_data <- process_meteorite_file("data/meteorite_landings.csv")
