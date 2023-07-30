15eb2d949d9b7b063f229b21d557dc8ad4298fb6
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-30 13:39:03 +0100

    initial commit
    Get the database credentials

---
+++ meteorites/process_database.R
@@ -0,0 +1,16 @@
+# Author: Lesley Duff
+# Filename: process_database.R
+# Title: Read database data and combine with clean data
+# Date Created: 2023-07-30
+# Description:
+#  add the data in this table into your clean data. 
+# When you’ve added the extra information into the table then write it to a CSV 
+# in the clean data folder.
+# Meteors Continued - PDA
+
+
+library(RPostgres)
+# source credentials file
+# this makes variables for databse ceredentials available to use
+source("meteorites_credentials.R")
+
