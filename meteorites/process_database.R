c530e3ab489a7d2d8a6a6c241a021e7762f25d55
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-30 13:46:57 +0100

    Sample connect/disconnect code

--- meteorites/process_database.R
+++ meteorites/process_database.R
@@ -14,3 +14,20 @@ library(RPostgres)
 # this makes variables for database credentials available to use
 source("meteorites_credentials.R")
 
+# create connection object using meteorite database credentials
+db_connection = dbConnect(
+  drv = Postgres(), 
+  host = meteorites_host,
+  port = meteorites_port,
+  dbname = meteorites_database,
+  user = meteorites_username,
+  password = meteorites_password,
+  bigint = "numeric"
+)
+
+# now we have our connection, don't need credentials anymore
+# remove them from global environment
+rm(meteorites_host, meteorites_port, meteorites_database, meteorites_username, 
+   meteorites_password)
+
+dbDisconnect(conn = db_connection)
