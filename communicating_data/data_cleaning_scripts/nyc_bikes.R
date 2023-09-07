a72ed04b9d3c89c7bba4736cefbcd641ba7ce31b
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-09-07 15:18:43 +0100

    Added code to clean data
    Will need to rejig presentation to use clean data file instead

--- communicating_data/data_cleaning_scripts/nyc_bikes.R
+++ communicating_data/data_cleaning_scripts/nyc_bikes.R
@@ -1,19 +1,38 @@
-
+library(tidyverse)
 library(tsibbledata)
 
 # Load data set ----
 # ?nyc_bikes comes from the tsibbledata package
 nyc_bikes_df <- nyc_bikes
+# View(nyc_bikes_df)
 
-names(nyc_bikes_df)
+#names(nyc_bikes_df)
+# "bike_id"       "start_time"    "stop_time"     "start_station" "start_lat"
+# "start_long"    "end_station"   "end_lat"       "end_long"      "type"
+# "birth_year"    "gender"
 
-# rename columns and select necessary columns
-nyc_bikes_clean <- nyc_bikes_df %>%
-  # add an age column
-  mutate(age = 2018 - birth_year)
-#  rename(
-#    common_name = species_common_name_taxon_age_sex_plumage_phase,
-#    scientific_name = species_scientific_name_taxon_age_sex_plumage_phase,
-#  ) %>%
-#  select(record_id, common_name, scientific_name, species_abbreviation, count)
-nyc_bikes_clean
+# Generate dataset containing only the variables of interest
+nyc_bikes_minimum <- nyc_bikes_df %>%
+  # add an age column for convenience in plots
+  mutate(age = 2018 - birth_year) %>%
+  select(age) # Select only columns needed for analysis
+
+rm(nyc_bikes_df)
+
+# replace observations in the age column where ages >=130
+# I strongly suspect the ages were typing mistakes typing 8 instead of 9
+# however we do not have knowledge of how it was obtained
+# There are only 2 entries so we are choosing to drop these couple of rows
+nyc_bikes_clean_age <- nyc_bikes_minimum[which(nyc_bikes_minimum$age < 130),]
+
+rm(nyc_bikes_minimum)
+
+# Check that ages look more believable
+#nyc_bikes_clean_age %>%
+#  ggplot() +
+#  geom_histogram(aes(x = age))
+
+# write clean data to csv
+write_csv(nyc_bikes_clean_age, "clean_data/nyc_bikes_clean.csv")
+
+rm(nyc_bikes_clean_age)
