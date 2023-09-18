93fce147df0970ab7ba922e87e23dc47d974c3e9
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-09-18 19:57:26 +0100

    Q1

--- penguins_analysis/data_cleaning_scripts/penguins.R
+++ penguins_analysis/data_cleaning_scripts/penguins.R
@@ -1,13 +1,37 @@
 # load in libraries
+library(janitor)
 library(palmerpenguins)
 library(tidyverse)
 
 # Load in data
 penguin_data <- palmerpenguins::penguins 
 
+# Initial examination of dataset
+#dim(penguins)
 
+#glimpse(penguins)
+
+#skimr::skim(penguins)
+
+#penguins %>% 
+#  count(species)
+
+# Take a closer look at the NA rows
+#penguins %>% 
+#  filter(is.na(bill_length_mm))
+
+# Which species of penguin are we interested in?
+penguin_species <-  c("Adelie", "Chinstrap", "Gentoo") 
+
+
+# Just two rows with NAs - drop them
+penguin_data_clean <- penguin_data %>%
+  filter(!is.na(bill_length_mm)) %>% 
+  # Check that we only take the species of interest
+  filter(species %in% penguin_species) %>% 
+  select(species, bill_length_mm)
 
 
 # write data to csv
-write.csv(penguin_data, "clean_data/penguins.csv")
+write.csv(penguin_data_clean, "clean_data/penguins.csv")
 
